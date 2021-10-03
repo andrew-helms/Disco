598739fef80112855e2ffc752f96be20c9cba3b5
andrew-helms <helmsandrew3@gmail.com>
2021-10-02 21:55:36 -0400

    Added frisbee mesh

--- Source/Discs/DiscsProjectile.cpp
+++ Source/Discs/DiscsProjectile.cpp
@@ -2,11 +2,10 @@
 
 #include "DiscsProjectile.h"
 #include "GameFramework/ProjectileMovementComponent.h"
-#include "Components/SphereComponent.h"
 
 ADiscsProjectile::ADiscsProjectile() 
 {
-	// Use a sphere as a simple collision representation
+	/*// Use a sphere as a simple collision representation
 	CollisionComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
 	CollisionComp->InitSphereRadius(5.0f);
 	CollisionComp->BodyInstance.SetCollisionProfileName("Projectile");
@@ -15,20 +14,33 @@ ADiscsProjectile::ADiscsProjectile()
 	// Players can't walk on it
 	CollisionComp->SetWalkableSlopeOverride(FWalkableSlopeOverride(WalkableSlope_Unwalkable, 0.f));
 	CollisionComp->CanCharacterStepUpOn = ECB_No;
+	CollisionComp->*/
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("DiscMesh"));
+	Mesh->AttachTo(RootComponent);
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> MeshAsset(TEXT("StaticMesh'/Game/Geometry/Meshes/Frisbee.Frisbee'"));
+	if (MeshAsset.Succeeded())
+	{
+		Mesh->SetStaticMesh(MeshAsset.Object);
+	}
+	Mesh->OnComponentHit.AddDynamic(this, &ADiscsProjectile::OnHit);
+	Mesh->BodyInstance.SetCollisionProfileName("Projectile");
+	Mesh->SetWalkableSlopeOverride(FWalkableSlopeOverride(WalkableSlope_Unwalkable, 0.f));
+	Mesh->CanCharacterStepUpOn = ECB_No;
 
 	// Set as root component
-	RootComponent = CollisionComp;
+	RootComponent = Mesh;
 
 	// Use a ProjectileMovementComponent to govern this projectile's movement
 	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileComp"));
-	ProjectileMovement->UpdatedComponent = CollisionComp;
-	ProjectileMovement->InitialSpeed = 5000.f;
+	ProjectileMovement->UpdatedComponent = Mesh;
+	ProjectileMovement->InitialSpeed = 3000.f;
 	ProjectileMovement->MaxSpeed = 30000.f;
 	ProjectileMovement->bRotationFollowsVelocity = true;
 	ProjectileMovement->bShouldBounce = true;
 
 	// Die after 3 seconds by default
-	InitialLifeSpan = 3.0f;
+	InitialLifeSpan = 300.0f;
 }
 
 
@@ -37,7 +49,7 @@ void ADiscsProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, U
 	// Only add impulse and destroy projectile if we hit a physics
 	if ((OtherActor != NULL) && (OtherActor != this) && (OtherComp != NULL) && OtherComp->IsSimulatingPhysics())
 	{
-		OtherComp->AddImpulseAtLocation(GetVelocity() * 1000.0f, GetActorLocation());
+		OtherComp->AddImpulseAtLocation(GetVelocity() * 100000.0f, GetActorLocation());
 
 		Destroy();
 	}
--- Source/Discs/DiscsProjectile.h
+++ Source/Discs/DiscsProjectile.h
@@ -12,12 +12,15 @@ class ADiscsProjectile : public AActor
 	GENERATED_BODY()
 
 	/** Sphere collision component */
-	UPROPERTY(VisibleDefaultsOnly, Category=Projectile)
-	class USphereComponent* CollisionComp;
+	/*UPROPERTY(VisibleDefaultsOnly, Category=Projectile)
+	class USphereComponent* CollisionComp;*/
 
 	/** Projectile movement component */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
 	class UProjectileMovementComponent* ProjectileMovement;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = Projectile)
+	class UStaticMeshComponent* Mesh;
 
 public:
 	ADiscsProjectile();
@@ -27,7 +30,7 @@ public:
 	void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
 
 	/** Returns CollisionComp subobject **/
-	FORCEINLINE class USphereComponent* GetCollisionComp() const { return CollisionComp; }
+	FORCEINLINE class UStaticMeshComponent* GetCollisionComp() const { return Mesh; }
 	/** Returns ProjectileMovement subobject **/
 	FORCEINLINE class UProjectileMovementComponent* GetProjectileMovement() const { return ProjectileMovement; }
 };
